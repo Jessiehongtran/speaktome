60028ee6f0ae17350191edba19ddb88c1fe22be7
Your Name <you@example.com>
2020-02-18 17:26:07 -0800

    added more videos

--- app/src/data/links.js
+++ app/src/data/links.js
@@ -2,61 +2,182 @@ export const links = [
     {   
         id: 1,
         clip: "https://www.youtube.com/embed/yqcevP1eAbE",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 2,
         clip: "https://www.youtube.com/embed/UF8uR6Z6KLc",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 3,
         clip: "https://www.youtube.com/embed/zPx5N6Lh3sw",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 4,
         clip: "https://www.youtube.com/embed/BmYv8XGl-YU",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 5,
         clip: "https://www.youtube.com/embed/_QhY6EAdeJ0",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 6,
         clip: "https://www.youtube.com/embed/xFFs9UgOAlE",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 7,
         clip: "https://www.youtube.com/embed/R35gWBtLCYg",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 8,
         clip: "https://www.youtube.com/embed/6hfOvs8pY1k",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 9,
         clip: "https://www.youtube.com/embed/JzOm6le-0jM",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 10,
         clip: "https://www.youtube.com/embed/QObAkF1_6CE",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 11,
         clip: "https://www.youtube.com/embed/C5pKtnmHTxg",
-        title: ""
+        title: "",
+        topic: ""
     },
     {
         id: 13,
         clip: "https://www.youtube.com/embed/Na8m4GPqA30",
-        title: ""
-    }
+        title: "",
+        topic: ""
+    },
+    {
+        id: 15,
+        clip: "https://www.youtube.com/embed/_QhY6EAdeJ0",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 16,
+        clip: "https://www.youtube.com/embed/tFJSWrscqag",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 17,
+        clip: "https://www.youtube.com/embed/0O2Rq4HJBxw",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 18,
+        clip: "https://www.youtube.com/embed/DD54J5kecpg",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 19,
+        clip: "https://www.youtube.com/embed/5pW2b1vwwf4",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 20,
+        clip: "https://www.youtube.com/embed/5MgBikgcWnY",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 21,
+        clip: "https://www.youtube.com/embed/EXaEz0mJXWY",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 22,
+        clip: "https://www.youtube.com/embed/EJ4B2uc9-CY",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 23,
+        clip: "https://www.youtube.com/embed/MxZpaJK74Y4",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 24,
+        clip: "https://www.youtube.com/embed/464puoD09dM",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 25,
+        clip: "https://www.youtube.com/embed/F2dFiK3wkRA",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 26,
+        clip: "https://www.youtube.com/embed/ZZ3qnxhMaoA",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 27,
+        clip: "https://www.youtube.com/embed/VH35Iz9veM0",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 28,
+        clip: "https://www.youtube.com/embed/ZIcQwDkcaWk",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 29,
+        clip: "https://www.youtube.com/embed/k9zTr2MAFRg",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 30,
+        clip: "https://www.youtube.com/embed/IgKWPdJWuBQ",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 31,
+        clip: "https://www.youtube.com/embed/usSfAVVCjaw",
+        title: "",
+        topic: ""
+    },
+    {
+        id: 32,
+        clip: "https://www.youtube.com/embed/MIHep4PNkNo",
+        title: "",
+        topic: ""
+    },
+   
 ]
